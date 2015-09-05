3fd16665581c02e1175041da115456e300b4d916
sfedyna <slfedyna@gmail.com>
2015-09-05 22:41:32 +0300

    upload project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/admin/');
+define('HTTPS_CATALOG', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Server/wamp/www/famaly_flowers/admin/');
+define('DIR_SYSTEM', 'C:/Server/wamp/www/famaly_flowers/system/');
+define('DIR_LANGUAGE', 'C:/Server/wamp/www/famaly_flowers/admin/language/');
+define('DIR_TEMPLATE', 'C:/Server/wamp/www/famaly_flowers/admin/view/template/');
+define('DIR_CONFIG', 'C:/Server/wamp/www/famaly_flowers/system/config/');
+define('DIR_IMAGE', 'C:/Server/wamp/www/famaly_flowers/image/');
+define('DIR_CACHE', 'C:/Server/wamp/www/famaly_flowers/system/cache/');
+define('DIR_DOWNLOAD', 'C:/Server/wamp/www/famaly_flowers/system/download/');
+define('DIR_UPLOAD', 'C:/Server/wamp/www/famaly_flowers/system/upload/');
+define('DIR_LOGS', 'C:/Server/wamp/www/famaly_flowers/system/logs/');
+define('DIR_MODIFICATION', 'C:/Server/wamp/www/famaly_flowers/system/modification/');
+define('DIR_CATALOG', 'C:/Server/wamp/www/famaly_flowers/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'famaly_flowers');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Server/wamp/www/famaly_flowers/catalog/');
+define('DIR_SYSTEM', 'C:/Server/wamp/www/famaly_flowers/system/');
+define('DIR_LANGUAGE', 'C:/Server/wamp/www/famaly_flowers/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Server/wamp/www/famaly_flowers/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Server/wamp/www/famaly_flowers/system/config/');
+define('DIR_IMAGE', 'C:/Server/wamp/www/famaly_flowers/image/');
+define('DIR_CACHE', 'C:/Server/wamp/www/famaly_flowers/system/cache/');
+define('DIR_DOWNLOAD', 'C:/Server/wamp/www/famaly_flowers/system/download/');
+define('DIR_UPLOAD', 'C:/Server/wamp/www/famaly_flowers/system/upload/');
+define('DIR_MODIFICATION', 'C:/Server/wamp/www/famaly_flowers/system/modification/');
+define('DIR_LOGS', 'C:/Server/wamp/www/famaly_flowers/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'famaly_flowers');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
